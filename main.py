48930961f67c1e22eb9170ef8d34d093e4744e8d
farzinrasht <farzinrasht@gmail.com>
2026-07-25 11:21:13 +0330

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ def handle_message(message):
         
         # استفاده از متد جدید گوگل برای دریافت پاسخ
         response = client.models.generate_content(
-            model='gemini-2.5-flash-lite',
+            model='gemini-2.5-flash',
             contents=full_prompt,
         )
         
